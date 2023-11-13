471c6457f904506020a0cc3995f9151169357fb8
acaldas <acaldas@powerhouse.inc>
2023-11-13 17:50:53 +0000

    fixed error when creating drive with id with invalid characters for a filesystem folder fixed eliminate folder bug

--- src/services/document-drive/electron-document-drive.ts
+++ src/services/document-drive/electron-document-drive.ts
@@ -302,7 +302,7 @@ class ElectronDocumentDrive implements IDocumentDrive {
     }
 
     async addDrive(input: AddDriveInput) {
-        await fs.mkdir(this.getPath(input.id));
+        await fs.mkdir(this.getPath(input.id), { recursive: true });
         this.document.addDrive(input);
         this.save();
     }
